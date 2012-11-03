ec2fa745287d7ff3ed8bee357f8dd6188d863816
rapido <robbert.van.dalen@gmail.com>
2012-11-03 21:02:55 +0100

    start of implementation

--- src/spread/Types.scala
+++ src/spread/Types.scala
@@ -2,5 +2,15 @@ package spread
 
 object Types {
 
+  // very abstract interface that covers all use cases //
+  trait Relation[A,B] {
+    def create(a: A, b: Set[B]): Relation[A,B]
+    def apply(a: A): Relation[B,B]
 
+    def size: Long
+    def flip: Relation[B,A]
+
+    def split(a: A): (Relation[A,B],Relation[A,B])
+    def union(o: Relation[A,B]): Relation[A,B]
+  }
 }
