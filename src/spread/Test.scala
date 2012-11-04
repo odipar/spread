6cdb629949f848e48b552bc5b4196433e436078d
rapido <robbert.van.dalen@gmail.com>
2012-11-04 20:16:54 +0100

    clone from the oer framework

--- src/spread/Test.scala
+++ src/spread/Test.scala
@@ -2,13 +2,31 @@ package spread
 
 object Test {
   import Types._
+  import OrderedSetImplementation._
+  import OrderedTreapSet._
+
+  implicit def intToIExpr[B](i: Int): IntExpr[B] = IExpr[B](i)
+
+  /*type ST[X] = OrderedISetImpl[X, Any, STreap[X, Any], STreapContext[X, Any]]
+
+  trait SSetImpl[A] extends OrderedISetImpl[A,Any,ST[A],DefaultSTreapContext[A],SSetImpl[A]]
+    */
 
   // TODO: Memoization of bind
   // TODO: Memoization of evaluation
+  // TODO: Memoization of bindings
+
+  def main(args: Array[String]): Unit = {
+    def i = (2:IntExpr[Any]) * 3 + 4
+    println("i: " + i)
+    println("i.e: " + i.evaluate)
+  }
 
   trait IntExpr[B] extends Expr[Int,B] {
     def bind(s: SSet[Binding[Int,B]]) = sys.error("not yet")
     def bindings: SSet[Binding[Int,B]] = sys.error("not yet")
+    def +(i: IntExpr[B]) = IAdd(this,i)
+    def *(i: IntExpr[B]) = IMul(this,i)
   }
 
   case class IExpr[B](i: Int) extends IntExpr[B] {
@@ -27,5 +45,9 @@ object Test {
       case (IExpr(i1),IExpr(i2)) => IExpr(i1*i2)
       case _ => this
     }
+  }
+
+  case class IBinding[B](label: B, labeled: IntExpr[B]) extends IntExpr[B] with Binding[Int,B] {
+    lazy val evaluate: IntExpr[B] = IBinding(label,labeled.evaluate.asInstanceOf[IntExpr[B]])
   }
 }
--- src/spread/Types.scala
+++ src/spread/Types.scala
@@ -3,17 +3,15 @@ package spread
 // very abstract types that cover all use cases //
 
 object Types {
-
-  def main(args: Array[String]): Unit = {}
+  import OrderedSetImplementation._
 
   // immutable sorted set
   trait SortedSet[A,S <: SortedSet[A,S]] {
     def create(a: A): S                 // must be O(1)
-    def equals(o: S): Boolean           // must be O(1)
+    def equalsTo(o: S): Boolean         // must be O(1)
 
-    def apply(a: A): Option[A]          // maximally log(size)
-    def size: Long                      // maximally log(size)
-    def split(a: A): (S, S)             // maximally log(size)
+    def get(a: A): Option[A]            // maximally log(size)
+    def split(a: A): (S,Option[A],S)    // maximally log(size)
 
     def union(o: S): S                  // if a < b - maximally log(min(a.size,b.size))
     def order(a1: A, a2: A): Int        // orders two elements a < b that are the SortedSet
