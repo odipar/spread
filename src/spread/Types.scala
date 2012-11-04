6ee872ab75d349e65e1ce88deac5288631a902d4
rapido <robbert.van.dalen@gmail.com>
2012-11-04 15:36:33 +0100

    expressions with labels

--- src/spread/Types.scala
+++ src/spread/Types.scala
@@ -1,10 +1,13 @@
 package spread
 
+import com.sun.org.apache.xpath.internal.operations.Variable
+
 object Types {
   // very abstract types that cover all use cases //
 
   def main(args: Array[String]): Unit = {}
 
+  // immutable sorted set
   trait SortedSet[A,S <: SortedSet[A,S]] {
     def create(a: A): S                 // must be O(1)
     def equals(o: S): Boolean           // must be O(1)
@@ -24,30 +27,29 @@ object Types {
     def range: SSet[B]
   }
 
+  // immutable binary relation
   trait Relation[A,B] extends SortedSet[MultiPair[A,B],Relation[A,B]] {
     def flip: Relation[B,A]
   }
 
-  trait Name {
-    def compare(o: Name): Int
+  // immutable expressions
+  trait Expr[A,B] {
+    def empty: Expr[A,B]
+    def evaluate: Expr[A,B]
+    def bind(b: SSet[Binding[A,B]]): Expr[A,B]
+    def bindings: SSet[Binding[A,B]]
   }
 
-  trait Expr[A] {
-    def evaluate: Expr[A]
-    def variables: SSet[Var[A]]
-    def bind(n: Name, e: Expr[A]): Expr[A]
+  trait Binding[A,B] extends Expr[A,B] {
+    def label: B
+    def labeled: Expr[A,B]
   }
 
-  trait Var[A] extends Expr[A] {
-    def name: Name
-    def expression: Expr[A]
+  trait UnaryOperator[A,B] extends Expr[A,B] {
+    def first: Expr[A,B]
   }
 
-  trait UnaryOperator[A] extends Expr[A] {
-    def first: Expr[A]
-  }
-
-  trait BinaryOperator[A] extends UnaryOperator[A] {
-    def second: Expr[A]
+  trait BinaryOperator[A,B] extends UnaryOperator[A,B] {
+    def second: Expr[A,B]
   }
 }
