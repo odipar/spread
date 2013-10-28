d0b2e926c02e59c532021ebdc19a48d4fc381f4f
rapido <robbert.van.dalen@gmail.com>
2013-10-28 16:22:32 +0100

    barbage collectable incremental tracing memoization

--- src/spread/IncrementalMemoization.scala
+++ src/spread/IncrementalMemoization.scala
@@ -2,60 +2,38 @@ package spread
 
 import java.lang.ref.WeakReference
 
-/* A new incremental memoization algorithm */
+/*
+  SPREAD: Garbage collectable incremental tracing memoization
+
+  Features:
+
+  1) spreadsheet-like incremental computation
+  2) re-use of expensive (sub)computations
+  3) full traceability
+  4) purely functional
+  5) automatic garbage collection of unused memoized traces
+
+  Copyright 2013: Robbert van Dalen
+*/
 
 object IncrementalMemoization {
   import language.implicitConversions
-
   import java.math.BigInteger
 
-  var totalt: Long = 0
+  var totalt: Long = 0 // Statistics
 
+  // Types
   trait Trace[R] {
-    synchronized { totalt = totalt + 1 } // statistics
-
     def apply(): R
     def step: Trace[R] = this
+
+    synchronized { totalt = totalt + 1 }
   }
 
   type T[X] = Trace[X]
   type TInt = T[BigInteger]
 
-  case class CInt(i: BigInteger) extends TInt {
-    def apply = i
-
-    def *(a: CInt) = cint(i multiply a.i)
-    def +(a: CInt) = cint(i add a.i)
-    def -(a: CInt) = cint(i subtract a.i)
-    def <(a: CInt) = compare(a) < 0
-    def >(a: CInt) = compare(a) > 0
-    def compare(a: CInt) = i.compareTo(a.i)
-
-    override def toString = i.toString
-  }
-
-  val mtable = scala.collection.mutable.WeakHashMap.empty[T[_], WeakReference[T[_]]]
-
-  def memoize[R](t: T[R]): T[R] = {
-    if (!mtable.contains(t)) mtable.put(t,new WeakReference(t))
-    mtable.get(t).get.get.asInstanceOf[T[R]]
-  }
-
-  def cint(i: Long): CInt = cint(BigInteger.valueOf(i))
-  def cint(i: BigInteger): CInt = CInt(i)
-
-  def call1[A1,A2,R](f: T[A1] => T[R], a1: T[A1]): T[R] = memoize(UnaTrace(f,a1))
-  def call2[A1,A2,R](f: (T[A1],T[A2]) => T[R], a1: T[A1], a2: T[A2]): T[R] = memoize(BinTrace(f,a1,a2))
-
-  def t_call1[A1,A2,R](f: A1 => R, a1: T[A1]): T[R] = memoize(TUnaTrace(f,a1))
-  def t_call2[A1,A2,R](f: (A1,A2) => R, a1: T[A1], a2: T[A2]): T[R] = memoize(TBinTrace(f,a1,a2))
-
-  // sugar
-  def $[A1,A2,R](f: T[A1] => T[R], a1: T[A1]) = call1(f,a1)
-  def $[A1,A2,R](f: (T[A1],T[A2]) => T[R], a1: T[A1], a2: T[A2])= call2(f,a1,a2)
-  implicit def toCInt(i: Int): CInt = cint(i)
-  implicit def toCInt(i: BigInteger): CInt = cint(i)
-
+  // Data
   case class UnaTrace[A1,R](f: T[A1] => T[R],a1: T[A1]) extends T[R] {
     override lazy val apply = step.apply
     override lazy val step: T[R] = f(a1)
@@ -74,11 +52,50 @@ object IncrementalMemoization {
     override lazy val apply = f(a1(),a2())
   }
 
+  case class CInt(i: BigInteger) extends TInt {
+    def apply = i
+
+    // Convenience
+    def *(a: CInt) = cint(i multiply a.i)
+    def +(a: CInt) = cint(i add a.i)
+    def -(a: CInt) = cint(i subtract a.i)
+    def <(a: CInt) = compare(a) < 0
+    def >(a: CInt) = compare(a) > 0
+    def compare(a: CInt) = i.compareTo(a.i)
+
+    override def toString = i.toString
+  }
+
+  // Implementation
+  def call1[A1,A2,R](f: T[A1] => T[R], a1: T[A1]): T[R] = memoize(UnaTrace(f,a1))
+  def call2[A1,A2,R](f: (T[A1],T[A2]) => T[R], a1: T[A1], a2: T[A2]): T[R] = memoize(BinTrace(f,a1,a2))
+
+  def t_call1[A1,A2,R](f: A1 => R, a1: T[A1]): T[R] = memoize(TUnaTrace(f,a1))
+  def t_call2[A1,A2,R](f: (A1,A2) => R, a1: T[A1], a2: T[A2]): T[R] = memoize(TBinTrace(f,a1,a2))
+
+
+  // Memoization
+  val mtable = scala.collection.mutable.WeakHashMap.empty[T[_], WeakReference[T[_]]]
+
+  def memoize[R](t: T[R]): T[R] = synchronized {
+    if (!mtable.contains(t)) mtable.put(t,new WeakReference(t))
+    mtable.get(t).get.get.asInstanceOf[T[R]]
+  }
+
+  // Sugar
+  def $[A1,A2,R](f: T[A1] => T[R], a1: T[A1]) = call1(f,a1)
+  def $[A1,A2,R](f: (T[A1],T[A2]) => T[R], a1: T[A1], a2: T[A2])= call2(f,a1,a2)
+  implicit def toCInt(i: Int): CInt = cint(i)
+  implicit def toCInt(i: BigInteger): CInt = cint(i)
+
+  // Convenience
+  def cint(i: Long): CInt = cint(BigInteger.valueOf(i))
+  def cint(i: BigInteger): CInt = CInt(i)
   val add = (a1: TInt, a2: TInt) => t_call2((x: BigInteger,y: BigInteger) => {x add y},a1,a2)
   val mul = (a1: TInt, a2: TInt) => t_call2((x: BigInteger,y: BigInteger) => {x multiply y},a1,a2)
 
-  // Examples
 
+  // Examples
   val fib: TInt => TInt = a1 => {
     val a = a1()
     if (a < 1) 1
@@ -89,5 +106,24 @@ object IncrementalMemoization {
     val a = a1()
     if (a < 1) 1
     else $(mul,a1,$(fac,a - 1))
+  }
+
+  final def test(): Unit = {}
+  {
+    var f1 = $(fib,100)
+    println("f1: " + f1())
+    println("tot: " + totalt)
+
+    var f2 = $(fib,110)
+    println("f2: " + f2())
+    println("tot: " + totalt)
+
+    f1 = null
+    f2 = null
+    System.gc() // removes (sub)traces from the weak memo table, held by f1 and f2
+
+    var f3 = $(fib,100)
+    println("f3: " + f3())
+    println("tot: " + totalt)
   }
 }
--- src/spread/Test.scala
+++ src/spread/Test.scala
@@ -1,73 +1,8 @@
 package spread
 
-import scala.util.parsing.input.CharSequenceReader
-
 /*
 
-a'      unbound label
-a'b     bound label
-"h"     char/string
-
-sequence and concat
-[a=b,b=c];[d=e,f=g] == b.c;e.g
-
-[0=a,1=b,2=c]       == a.b.c
-"h"."e"."l"."l"."o" == "hello"
-a.b;c.d             == a.b.c.d
-[a];c.d             == a.c.d
-
-
-GOAL SET OF OPERATORS
-
-UNARY:
- reduce:    [1 2 +] $                       => [(1 2 +,3)]
- wipe:      [(x'a,a)] #                     => [a=2]
- turn:      [a=b,c=d] %                     => [b=a,d=b]
- pack:      {1,2,3,4} <                     => 1.2.3.4
- unpack:    [a=1,b=2,c=d'3] >               => {a'1,b'2,c'(d'3)}
- foreach:   [1,2,3,4] 1 +@                  => [2,3,4,5]
- fold:      [1,2,3,4] . +                   => 10
- rfold      . [1.2.3.4] +                   => 1.3.6.10
-
-REFLECTION:
- backquery: [(1 2 +,3)] /t                   => [(1 2 +).3]
- bindings:  [a=x'(b'1 c'2 +)] /b             => [a=x`,x=b` c` +,b=1,c=1]
- dependencies:
-
- // [1,2,3] <\~\ => [[0=1],[0=2],[0=3]]
-
-x' unbound
-x'1 bound
-
-BINARY:
- add:      [a={2:1,2},b=2] [a={3:1,2},b={2,3},c=1] + => [a={5:1,2:2},b={2:2,3},c=1]
- subtract: [a={2:1,2},b=2] [a={3:1,2},b={2,3},c=1] - => ?
- multiply: [a={2:1,2},b=2] [a={3:1,2},b={2,3},c=1] * => [a={6:1,2},b=2,c=.]
- maximum:  [a={2:1,2},b=2] [a={3:1,2},b={2,3},c=1] | => [a={3:1,2},b=2,c=1]
- minimum:  [a={2:1,2},b=2] [a={3:1,2},b={2,3},c=1] & => [a={2:1,2},b=2,c=.]
- bind:     [a=b'3,c=d'4] [b={1,2},d=3] !             => [a=b'{1,2},c=d'3]
- match:    2010.x`.x` 2010.10.10 ?                   => [[x=10]] <-- VERY HARD TO IMPLEMENT!
-
-  ?+ (best match)
-  ?* (all matches)
-  ?& (first minimum match)
-  ?| (first maximum match)
-     etc. etc
-
- concat:   1.2;3.4                                   =>  1.2.3.4
-           "hello";"world"                           =>   "hello world"
-
-           "hello ";{"world","moon"}                 => {"hello world","hello moon"}
-
-           ["hello world"] [x`;"world"] ?
-
- [[a=1,b=2,c=3] x';[c=3] ?] = [[x=0],[x=[a=1]],[x=[a=1,a=2]],x=]   etc
- [0={6:0}];[0={7:0}] == [0={6:0},1={7:0}] == {6:0}.{7:0}
-
- DESIGN TO BE DECIDED:
-  Implement matching with leading and trailing?
-
-  a.b.x.d.e.f x;[2=x];y ? == [[x=a.b,y=[3=d,4=e,5=f]]]
+ Copyright 2013: Robbert van Dalen
 
  */
 
@@ -77,7 +12,6 @@ object Test {
   // Proof of concept works!
   final def main(args: Array[String]): Unit = {}
   {
-
     var f1 = $(fib,100)
     println("f1: " + f1())
     println("tot: " + totalt)
