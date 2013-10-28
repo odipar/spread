38b9f1c827023014e9260031a2adce6b52701a1e
rapido <robbert.van.dalen@gmail.com>
2013-10-28 16:23:36 +0100

    show case

--- src/spread/Test.scala
+++ src/spread/Test.scala
@@ -12,20 +12,6 @@ object Test {
   // Proof of concept works!
   final def main(args: Array[String]): Unit = {}
   {
-    var f1 = $(fib,100)
-    println("f1: " + f1())
-    println("tot: " + totalt)
-
-    var f2 = $(fib,110)
-    println("f2: " + f2())
-    println("tot: " + totalt)
-
-    f1 = null
-    f2 = null
-    System.gc()
-
-    var f3 = $(fib,100)
-    println("f3: " + f3())
-    println("tot: " + totalt)
+    test
   }
 }
