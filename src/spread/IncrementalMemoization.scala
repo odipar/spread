2eb5cd3142e7c5212cd335df6916446e72a5cb72
rapido <robbert.van.dalen@gmail.com>
2013-11-05 10:11:19 +0100

    visualization

--- src/spread/IncrementalMemoization.scala
+++ src/spread/IncrementalMemoization.scala
@@ -1,7 +1,7 @@
 package spread
 
 /*
-  SPREAD lib: Incremental Computational Reuse
+  SPREAD lib: Incremental Computation Reuse
 
   Features:
 
@@ -34,32 +34,38 @@ object IncrementalMemoization {
     def arg2: B
   }
 
-  // Compressing type signatures for convenience
-  type F1[-A,+R] = Function1[A,R]
-  type F2[-A,-B,+R] = Function2[A,B,R]
-  type FFCall[+R] = FCall[FCall[R]]
-
   // A lazily applied unary function call
-  case class LUCall[A,+R](f: F1[A,R], arg1: A) extends UCall[A,R] {
+  case class LUCall[A,+R](f: A => R, arg1: A)
+   extends UCall[A,R] {
+
     lazy val apply = f(arg1)
     override def toString = f + "(" + arg1 + ")"
   }
 
   // A lazily applied binary function call
-  case class LBCall[A,B,+R](f: F2[A,B,R], arg1: A, arg2: B) extends BCall[A,B,R] {
+  case class LBCall[A,B,+R](f: (A,B) => R, arg1: A, arg2: B)
+   extends BCall[A,B,R] {
+
     lazy val apply = f(arg1,arg2)
     override def toString = f + "(" + arg1 + "," + arg2 + ")"
   }
 
+  // A function call that yields another function call when evaluated
+  type FFCall[+R] = FCall[FCall[R]]
+
   // A lazily applied unary terminal function call, with one call argument
-  case class TUCall[A,B,+R](f: F1[A,R], a1: FFCall[A]) extends UCall[FCall[A],R] {
+  case class TUCall[A,+R](f: A => R, a1: FFCall[A])
+   extends UCall[FCall[A],R] {
+
     lazy val apply = f(arg1())
     lazy val arg1 = a1()
     override def toString = f + "(" + a1 + ")"
   }
 
   // A lazily applied binary terminal function call, with two call arguments
-  case class TBCall[A,B,+R](f: F2[A,B,R], a1: FFCall[A], a2: FFCall[B]) extends BCall[FCall[A], FCall[B],R] {
+  case class TBCall[A,B,+R](f: (A,B) => R, a1: FFCall[A], a2: FFCall[B])
+   extends BCall[FCall[A], FCall[B],R] {
+
     lazy val apply = f(arg1(),arg2())
     lazy val arg1 = a1()
     lazy val arg2 = a2()
@@ -67,20 +73,20 @@ object IncrementalMemoization {
   }
 
   // eager evaluation
-  def $[A,R](f: F1[A,R], a: A): R = f(a)
-  def $[A,B,R](f: F2[A,B,R], a: A, b: B): R = f(a,b)
+  def $[A,R](f: A => R, a: A): R = f(a)
+  def $[A,B,R](f: (A,B) => R, a: A, b: B): R = f(a,b)
 
   // lazy evaluation
-  def $$[A,R](f: F1[A,FCall[R]], a: A): FFCall[R] = LUCall(f,a)
-  def $$[A,B,R](f: F2[A,B,FCall[R]], a: A, b: B): FFCall[R] = LBCall(f,a,b)
-  def $$[A,R](f: F1[A,R], a1: FFCall[A]) = TUCall(f,a1)
-  def $$[A,B,R](f: F2[A,B,R], a1: FFCall[A], a2: FFCall[B]) = TBCall(f,a1,a2)
+  def $$[A,R](f: A => FCall[R], a: A): FFCall[R] = LUCall(f,a)
+  def $$[A,B,R](f: (A,B) => FCall[R], a: A, b: B): FFCall[R] = LBCall(f,a,b)
+  def $$[A,R](f: A => R, a1: FFCall[A]) = TUCall(f,a1)
+  def $$[A,B,R](f: (A,B) => R, a1: FFCall[A], a2: FFCall[B]) = TBCall(f,a1,a2)
 
   // hash-consed lazy evaluation (=lazy memoization)
-  def $_[A,R](f: F1[A,FCall[R]], a: A): FFCall[R] = hashcons($$(f,a))
-  def $_[A,B,R](f: F2[A,B,FCall[R]], a: A, b: B): FFCall[R] = hashcons($$(f,a,b))
-  def $_[A,R](f: F1[A,R], a1: FFCall[A]) = hashcons($$(f,a1))
-  def $_[A,B,R](f: F2[A,B,R], a1: FFCall[A], a2: FFCall[B]) = hashcons($$(f,a1,a2))
+  def $_[A,R](f: A => FCall[R], a: A): FFCall[R] = hashcons($$(f,a))
+  def $_[A,B,R](f: (A,B) => FCall[R], a: A, b: B): FFCall[R] = hashcons($$(f,a,b))
+  def $_[A,R](f: A => R, a1: FFCall[A]) = hashcons($$(f,a1))
+  def $_[A,B,R](f: (A,B) => R, a1: FFCall[A], a2: FFCall[B]) = hashcons($$(f,a1,a2))
 
   import scala.collection.mutable.WeakHashMap
   import java.lang.ref.WeakReference
--- src/spread/Test.scala
+++ src/spread/Test.scala
@@ -18,22 +18,22 @@ object Test {
 
 
   // Proof of concept works!
-  final def main(args: Array[String]): Unit = {}
+  final def main(args: Array[String]): Unit =
   {
+    val f1 = $$(sum4,Vector(1,2,3,4,5,6,7,8,9,10,11,12))
+    val f2 = $$(sum4,Vector(10,2,3,4,5,6,7,8,9,10,11,9))
 
-    println("sum4.1: " + $_(sum4,Vector(1,2,3,4,5,6,7,8))()())
-    println("sum4.2: " + $_(sum4,Vector(10,2,3,4,5,6,7,8))()())
-
-    /*
     val node1 = MyTreeNode(null,f1)
     val model1 = new DefaultTreeModel(node1)
     var tree1 = new JTree(model1)
+    tree1.setShowsRootHandles(true)
     tree1.setPreferredSize(new Dimension(600, 300))
     tree1.setMinimumSize(new Dimension(10, 10))
 
     val node2 = MyTreeNode(null,f2)
     val model2 = new DefaultTreeModel(node2)
     var tree2 = new JTree(model2)
+    tree2.setShowsRootHandles(true)
     tree2.setPreferredSize(new Dimension(600, 300))
     tree2.setMinimumSize(new Dimension(10, 10))
 
@@ -43,7 +43,7 @@ object Test {
     f.add(tree1, BorderLayout.EAST)
     f.add(tree2, BorderLayout.WEST)
     f.pack()
-    f.setVisible(true)              */
+    f.setVisible(true)
 
   }
 
@@ -58,13 +58,14 @@ object Test {
     }
   }
 
-  type Sum2Type = F1[Vector[Int],Int]
-  type AddType = F2[Int,Int,Int]
+  type AddType = (Int,Int) => Int
 
   lazy val add: AddType = new AddType {
     def apply(a1: Int, a2: Int): Int = a1 + a2
     override def toString = "+"
   }
+
+  type Sum2Type = Vector[Int] => Int
 
   lazy val sum2: Sum2Type = new Sum2Type {
     def apply(v: Vector[Int]): Int = {
@@ -77,6 +78,7 @@ object Test {
         add(sum2(l),sum2(r))
       }
     }
+    override def toString = "sum2"
   }
 
   lazy val sum3: Sum2Type = new Sum2Type {
@@ -90,13 +92,14 @@ object Test {
         $(add,$(sum3,l),$(sum3,r))
       }
     }
+    override def toString = "sum3"
   }
 
   type FInt = FCall[Int]
-  case class CInt(i: Int) extends NCall[Int] { def apply = i }
+  case class CInt(i: Int) extends NCall[Int] { def apply = i ; override def toString = i.toString}
   implicit def intToFInt(i: Int): FInt = CInt(i)
 
-  type Sum4Type = F1[Vector[Int],FInt]
+  type Sum4Type = Vector[Int] => FInt
 
   lazy val sum4: Sum4Type = new Sum4Type {
     def apply(v: Vector[Int]): FInt = {
@@ -106,20 +109,20 @@ object Test {
       else {
         val si = s / 2
         val (l,r) = v.splitAt(si)
-        $_(add,$$(sum4,l),$$(sum4,r))
+        $_(add,$_(sum4,l),$_(sum4,r))
       }
     }
     override def toString = "sum4"
   }
 
- /* case class MyTreeNode(parent: MyTreeNode, t: Trace[Any]) extends TreeNode {
+  case class MyTreeNode(parent: MyTreeNode, t: Any) extends TreeNode {
     import scala.collection.JavaConversions._
 
-    lazy val childs: Vector[TreeNode] = t.step match {
-      case UnaTrace(f,a1) => Vector[TreeNode](MyTreeNode(this, t.step))
-      case BinTrace(f,a1,a2) => Vector[TreeNode](MyTreeNode(this, t.step))
-      case TUnaTrace(f,a1) => Vector[TreeNode](MyTreeNode(this, a1.step))
-      case TBinTrace(f,a1,a2) => Vector[TreeNode](MyTreeNode(this, a1.step),MyTreeNode(this,a2.step))
+    lazy val childs: Vector[TreeNode] = t match {
+      case u: LUCall[_,_] => Vector[TreeNode](MyTreeNode(this, u.apply))
+      case b: LBCall[_,_,_] => Vector[TreeNode](MyTreeNode(this, b.apply))
+      case u: TUCall[_,_] => Vector[TreeNode](MyTreeNode(this, u.arg1))
+      case b: TBCall[_,_,_] => Vector[TreeNode](MyTreeNode(this, b.arg1), MyTreeNode(this, b.arg2))
       case _ => Vector[TreeNode]()
     }
 
@@ -132,5 +135,5 @@ object Test {
     def	isLeaf: Boolean = childs.size == 0
 
     override def toString = t.toString
-  }                                    */
+  }
 }
